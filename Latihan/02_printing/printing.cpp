4e3b45ccd74cca2431091b1b83628bccbe2aa12f
Camela Devs <camelaputri1@gmail.com>
2023-11-04 20:06:01 +0700

    Variable dan Deklarasi

---
+++ Latihan/02_printing/printing.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << endl;
+    cout << "Hello World \n";
+    
+    
+    
+    cin.get();
+    return 0;
+}
+
+// PRINTING
+/*
+
+    endl = menambah baris baru
+    \n = menambah baris baru
+
+    cout << "Hello World" << endl;
+    cout << "Hello World \n";
+
+*/
---
+++ Latihan/03_variable/variable.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 1;
+
+    cout << a << endl;
+
+    cin.get();
+    return 0;
+}
+
+// VARIABLE
+
+/*
+
+    Deklarasi seperti int a = 1;
+
+*/
